60da5bab4824b83f1e3ad9614b1b933b308a7bd1
Andrii_Grygoruk <andrii.grygoruk@racingpost.com>
2017-09-07 11:12:59 +0300

    tasks 4 - 6

--- src/arrays/Task04.java
+++ src/arrays/Task04.java
@@ -1,7 +1,34 @@
 package arrays;
 
+import java.util.Random;
+
 /**
  * Created by Andrii_Grygoruk on 9/7/2017.
  */
 public class Task04 {
+    public static void main(String[] args) {
+        int max = 10;
+        int min = 1;
+        Random rand = new Random();
+        int[] arr = new int[8];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt((max - min) + 1) + min;
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < arr.length; i++) {
+            if(i % 2 == 1){
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
+
+//4.	Создайте массив из 8 случайных целых чисел из отрезка [1;10].
+//        Выведите массив на экран в строку.
+//        Замените каждый элемент с нечётным индексом на ноль.
+//        Снова выведете массив на экран на отдельной строке.
--- src/arrays/Task06.java
+++ src/arrays/Task06.java
@@ -1,7 +1,37 @@
 package arrays;
 
+import java.util.Random;
+
 /**
  * Created by Andrii_Grygoruk on 9/7/2017.
  */
 public class Task06 {
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        int min = 10;
+        int max = 99;
+        Random rand = new Random();
+        int increment = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt((max - min) + 1) + min;
+            System.out.print(arr[i] + " ");
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]){
+                increment++;
+            }
+        }
+        System.out.println();
+        if (increment == 0){
+            System.out.println("Array is strictly increasing sequence");
+        }else{
+            System.out.println("Array is not strictly increasing sequence");
+        }
+    }
 }
+
+//6.	Создайте массив из 4 случайных целых чисел из отрезка [10;99],
+// выведите его на экран в строку. Определить и вывести на экран сообщение о том,
+// является ли массив строго возрастающей последовательностью.
